e93566714a2508ff7a19c3a4d7d5dc6965d531ba
Wojciech Chomanski <Wojciech_Chomanski.com@gmx.de>
2023-04-26 18:50:50 +0200

    Fift-techdegree-Project
    first upload of my Fift-Techdegree-Project.

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+baguetteBox.run('.gallery');
+
+const searchBox = document.querySelector("#search");
+const captions = document.querySelectorAll(".gallery a");
+
+searchBox.addEventListener("input",function() {
+const searchText = searchBox.value.toLowerCase();
+
+captions.forEach(function(caption) {
+  const captionText = caption.getAttribute("data-caption").toLowerCase();
+
+  if(captionText.includes(searchText)) {
+    caption.style.display = "block";
+  } else {
+    caption.style.display = "none";
+  }
+});
+});
